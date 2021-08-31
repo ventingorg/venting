ce3a9c7dd07c6d62f165989710aa5fd0ad150a5a
Garz4 <fancygarz4@gmail.com>
2021-08-31 12:07:07 -0500

    License to all files

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,18 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 Uriel Rivas
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * https://github.com/Garz4/venting/blob/main/LICENSE
+ */
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
